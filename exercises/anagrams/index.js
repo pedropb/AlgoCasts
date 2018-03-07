84a4cf9ac7d65766e39d2b64b6a46b88ffc13cef
Pedro Baracho <pedropb@trt3.jus.br>
2018-03-07 16:57:46 -0300

    Added anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,35 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    // remove everything but characters
+    stringA = stringA.replace(/[^a-zA-Z]/g, "");
+    stringB = stringB.replace(/[^a-zA-Z]/g, "");
+    
+    // make everything lower case
+    stringA = stringA.toLowerCase();
+    stringB = stringB.toLowerCase();
+
+    // count all chars on both strings
+    charCntStrA = {};
+    charCntStrB = {};
+    stringA.split('').forEach((letter) => charCntStrA[letter] ? charCntStrA[letter]++ : charCntStrA[letter] = 1 );
+    stringB.split('').forEach((letter) => charCntStrB[letter] ? charCntStrB[letter]++ : charCntStrB[letter] = 1 );
+
+    // compare both counts
+    // check if they have the same number of letters
+    if (Object.keys(charCntStrA).length !== Object.keys(charCntStrB).length) {
+        return false;
+    }
+
+    // now that we know that both hashes have the same amount of letters,
+    // we can compare them in any order.
+    for (let letter in charCntStrA) {
+        if (charCntStrB[letter] != charCntStrA[letter])
+            return false;
+    }
+
+    return true;
+}
 
 module.exports = anagrams;
