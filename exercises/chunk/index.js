75954a868ad86b07bbdd66a60cc2b4fc5200d647
Pedro Baracho <pedropbaracho@gmail.com>
2020-08-19 00:29:13 -0700

    Chunks solved

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,12 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let chunks = [];
+    while (array.length > 0) {
+        chunks.push(array.splice(0, size));
+    }
+    return chunks;
+}
 
 module.exports = chunk;
