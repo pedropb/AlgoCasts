ebb5926a00897ddf51f6fe44e45dda21b0ca79e0
Pedro Baracho <pedropbaracho@gmail.com>
2020-08-18 23:41:25 -0700

    Reverse string completed

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,8 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+    return str.split('').reduce((prev, curr) => curr + prev);
+}
 
 module.exports = reverse;
