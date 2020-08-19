6ee92dac882663e7cf784c7bf699bced088f97ed
Pedro Baracho <pedropbaracho@gmail.com>
2020-08-19 00:15:56 -0700

    Max char solved

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,19 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let countMap = {};
+    let maxCharCount = 0;
+    let currentMaxChar = '';
+    for (let char of str) {
+        countMap[char] = (countMap[char] || 0) + 1;
+        if (countMap[char] > maxCharCount) {
+            maxCharCount = countMap[char];
+            currentMaxChar = char;
+        }
+    }
+
+    return currentMaxChar;
+}
 
 module.exports = maxChar;
